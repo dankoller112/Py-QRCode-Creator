c1d899dbfb125a95084423354e5fe4ff106c4f76
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-24 13:46:17 +0100

    Add files via upload
    Initial commit

---
+++ qr-creator.py
@@ -0,0 +1,9 @@
+import qrcode
+
+# Create qr-code and configure size
+qr = qrcode.QRCode(box_size=50, border=2)
+qr.add_data("*Beliebigen Inhalt einfügen*")
+# Configure output and color schemes
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("Filename.png")
+img.show()
