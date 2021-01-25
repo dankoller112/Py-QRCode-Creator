8159ee99ecb55bb975fad28c7389ea4ca9c9c6ef
Daniel <dkoller02@gmail.com>
2021-01-25 18:50:48 +0100

    + Added import library "Image" from "PIL"

--- qr-creator.py
+++ qr-creator.py
@@ -1,8 +1,10 @@
 import qrcode
+from PIL import Image
 
 # Create qr-code and configure size
 qr = qrcode.QRCode(box_size=50, border=2)
 qr.add_data("*Beliebigen Inhalt einfügen*")
+
 # Configure output and color schemes
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("Filename.png")
